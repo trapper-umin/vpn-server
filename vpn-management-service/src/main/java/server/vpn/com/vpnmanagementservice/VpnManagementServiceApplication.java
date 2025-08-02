8ee49b57d68e76200b9022814ff05e4f6436db96
trapper <trapp.umin@gmail.com>
2025-08-02 16:24:31 +0300

    vpn-management-service

---
+++ vpn-management-service/src/main/java/server/vpn/com/vpnmanagementservice/VpnManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package server.vpn.com.vpnmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpnManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VpnManagementServiceApplication.class, args);
+    }
+
+}
---
+++ vpn-management-service/src/test/java/server/vpn/com/vpnmanagementservice/VpnManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package server.vpn.com.vpnmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpnManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
