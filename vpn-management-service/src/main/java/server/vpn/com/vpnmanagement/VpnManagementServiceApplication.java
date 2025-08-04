19e8abd6aaa479c511690cf449cf07da34adc754
trapper <trapp.umin@gmail.com>
2025-08-04 17:44:40 +0300

    vpn-management-service

--- vpn-management-service/src/main/java/server/vpn/com/vpnmanagement/VpnManagementServiceApplication.java
+++ vpn-management-service/src/main/java/server/vpn/com/vpnmanagement/VpnManagementServiceApplication.java
@@ -2,8 +2,10 @@ package server.vpn.com.vpnmanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VpnManagementServiceApplication {
 
     public static void main(String[] args) {
